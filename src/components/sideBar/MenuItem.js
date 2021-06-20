64b1c7cbe059dd41944b79481e26d3fb91b9c860
kfiddle <kenjfiddle@gmail.com>
2021-06-19 22:12:54 -0400

    various

--- src/components/sideBar/MenuItem.js
+++ src/components/sideBar/MenuItem.js
@@ -1,12 +1,29 @@
+import { useState, useEffect } from "react";
+
 import styles from "./MenuItem.module.css";
 
 const MenuItem = (props) => {
+  const [glowing, setGlowing] = useState(true);
+
+  const darkLetters = { color: "gold", fontSize: "2rem" };
 
   const clickHandler = () => {
     props.chosen(props.title);
   };
 
-  return <h2 onClick={clickHandler} className={styles.sideBarItem}>{props.title}</h2>;
+  useEffect(()=> {
+    const turnOffGlow = setTimeout(() => { setGlowing(false) }, 100)
+  }, [])
+
+  return (
+    <h2
+      onClick={clickHandler}
+      className={styles.sideBarItem}
+      style={glowing? darkLetters : null}
+    >
+      {props.title}
+    </h2>
+  );
 };
 
 export default MenuItem;
--- src/components/sideBar/SideBar.js
+++ src/components/sideBar/SideBar.js
@@ -5,27 +5,50 @@ import MenuItem from "./MenuItem";
 import styles from "./SideBar.module.css";
 
 const SideBar = (props) => {
-    const [sideBarPlace, setSideBarPlace] = useState(-25);
-
-
-    useEffect(()=> {
-        const slideIn = setTimeout(() => {
-            setSideBarPlace(10);
-
-        }, 150)
-
-
-    }, [])
+  const [sideBarPlace, setSideBarPlace] = useState(-25);
+  const [menu1, setMenu1] = useState("");
+  const [menu2, setMenu2] = useState("");
+  const [menu3, setMenu3] = useState("");
 
   const clicked = (choice) => {
     props.choice(choice);
   };
 
+  const list = <MenuItem title={"About Me"} chosen={clicked} />;
+
+  useEffect(() => {
+    const slideIn = setTimeout(() => {
+      setSideBarPlace(10);
+    }, 350);
+
+    const firstMenu = setTimeout(() => {
+        setMenu1(<MenuItem title={props.menuList[0]} chosen={clicked} />)
+    }, 1000)
+
+    const secondMenu = setTimeout(() => {
+        setMenu2(<MenuItem title={props.menuList[1]} chosen={clicked} />)
+    }, 1300)
+
+    const thirdMenu = setTimeout(() => {
+        setMenu3(<MenuItem title={props.menuList[2]} chosen={clicked} />)
+    }, 1600)
+    
+  }, []);
+
   const menuListToDisplay = props.menuList.map((menuItem) => (
     <MenuItem title={menuItem} chosen={clicked} key={Math.random()} />
   ));
 
-  return <div className={styles.sideBarDiv} style={{transform: `translateX(${sideBarPlace}vw)`}}>{menuListToDisplay}</div>;
+  return (
+    <div
+      className={styles.sideBarDiv}
+      style={{ transform: `translateX(${sideBarPlace}vw)` }}
+    >
+      {menu1}
+      {menu2}
+      {menu3}
+    </div>
+  );
 };
 
 export default SideBar;
