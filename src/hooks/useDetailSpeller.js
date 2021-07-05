a6a36385cff500107cba2795414815115b321a01
kfiddle <kenjfiddle@gmail.com>
2021-07-05 14:12:30 -0400

    dependency array adjustment

--- src/hooks/useDetailSpeller.js
+++ src/hooks/useDetailSpeller.js
@@ -24,7 +24,7 @@ const useDetailSpeller = (on, statementOn, statementOff) => {
     } else {
       setLetters(statementOff);
     }
-  }, [on, lettersIndex]);
+  }, [on, lettersIndex, statementOff, statementOn]);
 
   return letters;
 };
